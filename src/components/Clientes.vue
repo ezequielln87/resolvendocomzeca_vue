50b033d2d42f1e8e8742b3448ce00dbd4ff20590
ezequielln87 <ezequielln87@gmail.com>
2022-11-12 20:49:14 -0300

    cliente update

--- src/components/Clientes.vue
+++ src/components/Clientes.vue
@@ -2,6 +2,12 @@
 
 <template>
   <v-container>
+    <v-snackbar v-model="snackbar" :timeout="timeout" :color="color">
+      {{ message }}
+      <v-btn dark text @click="snackbar = false">
+        <v-icon>mdi-close</v-icon>
+      </v-btn>
+    </v-snackbar>
     <v-row>
       <v-col cols="12">
         <h1 class="display-1 font-weight-bold mb-3">Clientes</h1>
@@ -15,26 +21,32 @@
           </template>
           <v-card>
             <v-card-title>
-              <span class="headline"></span>
+              <span class="headline">{{ formTitle }}</span>
             </v-card-title>
             <v-card-text>
               <v-container>
                 <v-row>
                   <v-col cols="12">
                     <v-text-field
-                      v-model="clientes.nome"
+                      v-model="editedItem.nome"
                       label="Nome"
                     ></v-text-field>
                   </v-col>
                   <v-col cols="12">
                     <v-text-field
-                      v-model="clientes.email"
+                      v-model="editedItem.cpf"
+                      label="CPF"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12">
+                    <v-text-field
+                      v-model="editedItem.email"
                       label="Email"
                     ></v-text-field>
                   </v-col>
                   <v-col cols="12">
                     <v-text-field
-                      v-model="clientes.telefone"
+                      v-model="editedItem.telefone"
                       label="Telefone"
                     ></v-text-field>
                   </v-col>
@@ -43,8 +55,10 @@
             </v-card-text>
             <v-card-actions>
               <v-spacer></v-spacer>
-              <!-- <v-btn color="blue darken-1" text @click="dialog = false">Cancel</v-btn> -->
-              <!-- <v-btn color="blue darken-1" text @click="salvarCliente">Salvar</v-btn> -->
+              <v-btn color="blue darken-1" text @click="dialog = false"
+                >Cancel</v-btn
+              >
+              <v-btn color="blue darken-1" text @click="save">Salvar</v-btn>
             </v-card-actions>
           </v-card>
         </v-dialog>
@@ -62,6 +76,12 @@
           <template v-slot:no-data>
             <v-btn color="primary" @click="initialize"> Recargar </v-btn>
           </template>
+          <template v-slot:[`item.actions`]="{ item }">
+              <v-icon small class="mr-2" @click="editItem(item)">
+                mdi-pencil
+              </v-icon>
+              <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+            </template>
         </v-data-table>
       </v-col>
     </v-row>
@@ -69,15 +89,21 @@
 </template>
 
 <script>
-// clientes api get
-import axios from "axios";
-// nome, cpf, email, telefone
 
 export default {
   name: "ClientesList",
   data: () => ({
     dialog: false,
+    snackbar: false,
     clientes: [],
+    editedIndex: -1,
+    editedItem: {
+      nome: "",
+      cpf: "",
+      email: "",
+      telefone: ""
+    },
+    
     headers: [
       {
         text: "Nome",
@@ -106,12 +132,44 @@ export default {
     },
   },
 
-  
   methods: {
-    getClientes() {
-      this.$http.get("clientelist/").then((response) => {
-        this.clientes = response.data;
-      });
+    async getClientes() {
+      let response = await this.$http.get("clientelist/");
+      this.clientes = response.data;
+
+      // this.$http.get("clientelist/").then((response) => {
+      //   this.clientes = response.data;
+      // });
+    },
+
+    save() {
+      if (this.editedIndex > -1) {
+        Object.assign(this.clientes[this.editedIndex], this.editedItem);
+
+        this.$http.patch("clienteupdate/" + this.editedItem.id + "/", {
+          nome: this.editedItem.nome,
+          cpf: this.editedItem.cpf,
+          email: this.editedItem.email,
+          telefone: this.editedItem.telefone,
+        });
+      } else {
+        this.$http
+        .post("clientecreate/", this.editedItem)
+        .then(() => {
+            this.clientes.push(this.editedItem);
+            // this.clientes.push(response.data);
+            // this.dialog = false;
+            //success
+            this.snackbar = true;
+            this.message = "Cliente cadastrado com sucesso!";
+            this.color = "success";
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+      
+      this.dialog = false;
     },
 
     close() {
@@ -121,28 +179,32 @@ export default {
         this.editedIndex = -1;
       }, 300);
     },
-  },
-  //actions
-  editItem(item) {
-    alert("Edit " + item.nome);
-  },
-  deleteItem(item) {
-    alert("Delete " + item.nome);
-  },
-  //buttons
-  novoCliente() {
-    this.dialog = true;
-  },
 
-  eliminarCliente(id) {
-    console.log(id);
-    axios
-      .delete(`http://127.0.0.1:8000/api/clientedelete/${id}`)
-      .then((res) => {
-        console.log(res);
-        this.getClientes();
-      });
-    console.log("aham");
+    editItem(item) {
+      
+      this.editedIndex = this.clientes.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
+
+    },
+
+    deleteItem(item) {
+      const index = this.clientes.indexOf(item);
+      confirm("Você tem certeza que deseja deletar este item?") &&
+        this.clientes.splice(index, 1);
+      
+      this.$http({
+        method: "delete",
+        url: "clientedelete/" + item.id + "/",
+      }).then((response) => {
+        // cliente apagado com sucesso
+        this.snackbar = true;
+        this.message = "Cliente apagado com sucesso!";
+        this.color = "success";
+        console.log(response);
+      }); 
+    },
+
   },
 
   initialize() {
